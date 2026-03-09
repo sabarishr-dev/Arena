9eae45026a1e4e0673ef1700ac4f9de84fd43ef6
sabarishr-dev <sabarish.r@vinsinfo.com>
2026-03-09 18:46:46 +0530

    fix: patch react2shell vulnerability

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/l';
 import { useEffect } from 'react';
 import SignIn from '@/components/auth/SignIn';
 
